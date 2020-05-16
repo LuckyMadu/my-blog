c4348a25f2945236d14fbaba3c15baac6b885ea0
Lahiru Amarathunga <lahiruamarathunge94@gmail.com>
2020-05-16 23:01:29 +0530

    first commit

---
+++ src/build/precache-manifest.34edbc46c1e7cca4a112e88d3174a573.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed79d22d9eb7f85f9872e04c50e6dd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b75a4b1af60ea3c431d9",
+    "url": "/static/css/main.37f031ad.chunk.css"
+  },
+  {
+    "revision": "c2be464aa755dd558ed8",
+    "url": "/static/js/2.a38ab3ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a38ab3ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b75a4b1af60ea3c431d9",
+    "url": "/static/js/main.abecd4db.chunk.js"
+  },
+  {
+    "revision": "053c8e34507918769709",
+    "url": "/static/js/runtime-main.aecaeffa.js"
+  }
+]);
